590be855fd9953f83d718a84c24b319a63519b85
yuka-lamp <oonishi@lamp.jp>
2020-12-01 13:18:34 +0900

    update

--- footer.php
+++ footer.php
@@ -21,7 +21,7 @@ $wp_url = get_template_directory_uri(); ?>
 <?php if (is_page('kowarsu')): ?>
 <a class="fix-btn tel" href="tel:0120-133-064"><img src="<?php echo $wp_url; ?>/dist/images/fix_btn_tel_sp.png" alt="" srcset="<?php echo $wp_url; ?>/dist/images/fix_btn_tel_sp.png 1x, <?php echo $wp_url; ?>/dist/images/fix_btn_tel_sp@2x.png 2x"></a>
 <?php else: ?>
-<a class="fix-btn" href="<?php echo $home; ?>/ikkyuu/"><img src="<?php echo $wp_url; ?>/dist/images/fix_btn_sp.png" alt="" srcset="<?php echo $wp_url; ?>/dist/images/fix_btn_sp.png 1x, <?php echo $wp_url; ?>/dist/images/fix_btn_sp@2x.png 2x"></a>
+<a class="fix-btn" href="<?php echo $home; ?>/kowarsu/"><img src="<?php echo $wp_url; ?>/dist/images/fix_btn_sp.png" alt="" srcset="<?php echo $wp_url; ?>/dist/images/fix_btn_sp.png 1x, <?php echo $wp_url; ?>/dist/images/fix_btn_sp@2x.png 2x"></a>
 <?php endif; ?>
 <?php wp_footer(); ?>
 </body>
